7efe92824788c93089a206d1dbb1c8914b586132
aaron-quickstart <aaron12clure@gmail.com>
2025-04-19 19:44:38 -0400

    corrected request url

--- project4app/src/LogInScreen.jsx
+++ project4app/src/LogInScreen.jsx
@@ -5,11 +5,11 @@ import axios from 'axios';  // npm install axios react-router-dom
 
 export default function LoginScreen()
 {
-    const [formData, setFormData] = useState({username: '' , password: ''})
+    const [formData, setFormData] = useState({user_name: '' , user_password: ''})
 
     const handleSubmit = async(e) => {
         e.preventDefault();
-        const response = await axios.post('http://localhost:3000/getUsers' , formData);  //this Will be the URL of the API I think on riplet
+        const response = await axios.post('http://localhost:3000/login' , formData);  //this Will be the URL of the API I think on riplet
         if(response)
         {
             console.log(response);
@@ -22,9 +22,9 @@ export default function LoginScreen()
             <h2>Log in</h2>
             <form onSubmit={handleSubmit}>
                 <input type='text' placeholder='User Name' required
-                    onChange={(e) => setFormData({... formData, email: e.target.value})} />
+                    onChange={(e) => setFormData({... formData, user_name: e.target.value})} />
                 <input type='password' placeholder='Password' required
-                    onChange={(e) => setFormData({... formData, password: e.target.value})} />
+                    onChange={(e) => setFormData({... formData, user_password: e.target.value})} />
                     <button type='submit'>Login</button>
             </form>
         </>
